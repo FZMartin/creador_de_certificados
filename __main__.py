18981c11fc5deba6ce4da0b9067357fd0a994134
Juan Martin Pedrozo <pedrozo.juanma@gmail.com>
2021-07-27 12:18:14 -0300

    Creador funcionando

--- __main__.py
+++ __main__.py
@@ -1,12 +1,14 @@
 from classes.classes import *
 
 def main():
-    app = App(dest_path='',
-              word_template_path='',
-              csv_path='',
-              keywords_map=[])
+    app = App(dest_path='path\\to\\folder',
+              word_template_path='path\\to\\docx_template',
+              csv_path='path\\to\\csv',
+              keywords_map=['${nombre}', '${dni}'])
     
     app.create_certificates()
+    app.export_to_pdf()
+    
 
 if __name__ == '__main__':
     main()
